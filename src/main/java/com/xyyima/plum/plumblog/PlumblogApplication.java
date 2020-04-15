c379a09f31cb14e773b940b31f59aa67a1b65383
Tony Wang <guanwan@microsoft.com>
2020-04-15 13:30:50 +0800

    migrate to package war

---
+++ src/main/java/com/xyyima/plum/plumblog/PlumblogApplication.java
@@ -0,0 +1,13 @@
+package com.xyyima.plum.plumblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xyyima/plum/plumblog/PlumblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyyima.plum.plumblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
