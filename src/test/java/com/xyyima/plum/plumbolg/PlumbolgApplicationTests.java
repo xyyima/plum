caf8e139e11d88c88c8299ae752b708edb95be59
Tony Wang <guanwan@microsoft.com>
2020-04-15 11:46:42 +0800

    inti project with spring boot initial

---
+++ src/test/java/com/xyyima/plum/plumbolg/PlumbolgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyyima.plum.plumbolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumbolgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
